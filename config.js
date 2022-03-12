14619950122ae32f0d6ce0169580f606492c5a98
jiannorris <86064257+jiannorris@users.noreply.github.com>
2022-03-12 13:35:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const config = {
   style: "mapbox://styles/bereaepg/ckpwsckpf4sb417n33parhieh",
   accessToken:
     "pk.eyJ1IjoiYmVyZWFlcGciLCJhIjoiY2twcHJrdTQ2MDJ1aTJ2bzY4dWhmZG4wayJ9.3cIych-Xcf-apOrR1xlzaw",
-  CSV: "./March Map.csv",
+  CSV: "./ABR v3.csv",
   center: [-84.3, 37.6],
   zoom: 7,
-  title: "US Bike Route 21",
+  title: "Daniel Boone Bike Route 21",
   description:
     "This route follows US 21N from Cumberland Gap to Maysville You can search by category (lodging, dining, etc.) by city, or by county.",
   sideBarInfo: ["Name", "Address", "Phone"],
@@ -16,6 +16,7 @@ const config = {
       title: "Category: ",
       columnHeader: "Category",
       listItems: [
+        "Boone Trace Markers"
         "Points of Interest",
         "Lodging",
         "Dining",
@@ -35,10 +36,18 @@ const config = {
     },
     {
       type: "checkbox",
-      title: "Services available",
-      columnHeader: "Services available", // Case sensitive - must match spreadsheet entry
-      listItems: ["Restrooms", "Wi-Fi", "Cold beer"], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+      title: "Start",
+      columnHeader: "City", // Case sensitive - must match spreadsheet entry
+      listItems: ["Middlesboro", "London", "Berea", "Maysville"], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
+    {
+      type: "checkbox",
+      title: "Finish",
+      columnHeader: "City", // Case sensitive - must match spreadsheet entry
+      listItems: ["Middlesboro", "London", "Berea", "Maysville"], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+    
+    
     {
       type: "dropdown",
       title: "County: ",
